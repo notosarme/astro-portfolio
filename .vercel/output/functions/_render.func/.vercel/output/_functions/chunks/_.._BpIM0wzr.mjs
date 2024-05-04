27df58263ea1d3047e04e6aa65519d4d8b9d2820
notosarme <djinnstreasure@pm.me>
2024-05-04 15:22:39 -0400

    Remove unnecessary files

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_.._BpIM0wzr.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__DV42Pc1_.mjs').then(n => n.b);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/blog_Bz7iOm2T.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/blog_DRch_nze.mjs');
-
-export { page };
