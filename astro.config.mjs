f9a62e8af2ddf124d8836044cd21d0af118c5def
notosarme <djinnstreasure@pm.me>
2024-05-04 15:15:47 -0400

    Prepare for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: 'https://notosarme.github.io',
-  base: 'tj-portfolio',
+  output: "server",
+  adapter: vercel()
 });
